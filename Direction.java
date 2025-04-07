49ed477128c1b1f2c998f8c6a507df62671fa78e
laura <lauravtd44@gmail.com>
2025-04-06 21:20:42 -0400

    Initial sharing of project

---
+++ Direction.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Direction - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Direction //exercise 38
+{
+  NORTH, SOUTH, EAST, WEST
+}
